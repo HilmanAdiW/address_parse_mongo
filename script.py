6bf37aec9bb840ede2876959a009afe2627097da
hilman <extremehilman@gmail.com>
2020-02-23 14:29:21 +0700

    aing males sumpah

--- script.py
+++ script.py
@@ -9,14 +9,13 @@ from pprint import pprint
 client = MongoClient(port=27017)
 db = client.ptposAddress # mongodb database in server hadoop@demo.rosebaycorporate.com -p 3002
 
-start = 1
-batch_size = 100   # Batch size
+batch_size = 5   # Batch size
 counter = 0  # starting from 1 because we are ignoring the first column which is the header name
 # Counter is used to skip the batches
 i = 1
-while counter < 2:   # set the batch
-    # start = counter * batch_size
-    df = pd.read_csv('C:\\Users\\die_zer0\\hilman\\Data acquistion system\\alamat penerima januari reg 4 5.csv', header=None, names=['al_penerima'], skiprows=1, nrows=batch_size)
+while counter < 4:   # set the batch
+    start = (counter * batch_size)+1
+    df = pd.read_csv('C:\\Users\\die_zer0\\hilman\\Data acquistion system\\alamat penerima januari reg 4 5.csv', header=None, names=['al_penerima'], skiprows=start, nrows=batch_size)
     # start = counter * batch_size
     # update to start and end rows
     for address in df['al_penerima']:
