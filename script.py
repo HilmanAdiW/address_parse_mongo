0d461ccb04f1ff9aab653ec6952c846d3d7a358d
kelen <marbunellen@gmail.com>
2020-02-25 10:06:36 +0700

    changing batch size and looping requirement

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import numpy as np
 import pandas as pd
 import math
@@ -6,17 +12,21 @@ from pymongo import MongoClient
 from pprint import pprint
 from parserpackage import AddressParser
 
+
+# In[5]:
+
+
 # Connection to database
 client = MongoClient(port=27017)
 db = client.ptposAddress # mongodb database in server hadoop@demo.rosebaycorporate.com -p 3002
 
-batch_size = 5   # Batch size
-counter = 0  # starting from 1 because we are ignoring the first column which is the header name
+batch_size = 10    # Batch size
+counter = 1  # starting from 1 because we are ignoring the first column which is the header name
 # Counter is used to skip the batches
 i = 1
 while counter < 4:   # set the batch
     start = (counter * batch_size)+1
-    df = pd.read_csv('C:\\Users\\die_zer0\\hilman\\Data acquistion system\\alamat penerima januari reg 4 5.csv', header=None, names=['al_penerima'], skiprows=start, nrows=batch_size)
+    df = pd.read_csv(r'C:\Users\Lenovo\Downloads\Telegram Desktop\alamat penerima januari reg 4 5.csv', header=None, names=['al_penerima'], skiprows=start, nrows=batch_size)
     # start = counter * batch_size
     # update to start and end rows
     for address in df['al_penerima']:
@@ -34,3 +44,37 @@ while counter < 4:   # set the batch
         i+=1
         time.sleep(1)
     counter += 1
+
+
+# In[9]:
+
+
+# Connection to database
+client = MongoClient(port=27017)
+db = client.ptposAddress # mongodb database in server hadoop@demo.rosebaycorporate.com -p 3002
+
+batch_size = 100    # Batch size
+counter = 1  # starting from 1 because we are ignoring the first column which is the header name
+# Counter is used to skip the batches
+i = 1
+while counter <= 1:   # set the batch
+    start = (counter * batch_size)+1
+    df = pd.read_csv(r'C:\Users\Lenovo\Downloads\Telegram Desktop\alamat penerima januari reg 4 5.csv', header=None, names=['al_penerima'], skiprows=start, nrows=batch_size)
+    # start = counter * batch_size
+    # update to start and end rows
+    for address in df['al_penerima']:
+        data = {}
+        print('Data processing : {}'.format(i))
+        data['raw address'] = address
+        print('Raw address : {}'.format(address))
+
+    ####### Write a script to parse addresses using parser module
+        test_object = AddressParser()
+        data["parse address"] = test_object.parse_address(address)
+        result = db.addresses.insert_one(data)  # Save data in database
+        print('Data processed : {}'.format(i))
+
+        i+=1
+        time.sleep(1)
+    counter += 1
+
